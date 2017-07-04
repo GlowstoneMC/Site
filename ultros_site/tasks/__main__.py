f493f3876eb8a2835fa3f2e9c5deeaf917580338
Gareth Coles <gdude2002@gmail.com>
2017-07-04 10:10:39 +0000

    Remove old comments task definition

--- ultros_site/tasks/__main__.py
+++ ultros_site/tasks/__main__.py
@@ -10,7 +10,6 @@ app = Celery(
     broker="amqp://storage:5672/glowstone",
     backend="redis://storage:6379/2",
     include=[
-        "ultros_site.tasks.comments",
         "ultros_site.tasks.common",
         "ultros_site.tasks.discord",
         "ultros_site.tasks.email",
