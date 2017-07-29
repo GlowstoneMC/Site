f340ffa0d2118a02f464fe254a74a8473ddae11c
Gareth Coles <gdude2002@gmail.com>
2017-07-29 15:51:51 +0100

    Move Celery constants to an editable python file, as recommended

--- ultros_site/tasks/__main__.py
+++ ultros_site/tasks/__main__.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+from ultros_site.tasks import config
+
 from celery import Celery
 from celery.schedules import crontab
 
@@ -7,39 +9,12 @@ __author__ = "Gareth Coles"
 
 app = Celery(
     "ultros_site",
-    broker="amqp://storage:5672/glowstone",
-    backend="redis://storage:6379/2",
-    include=[
-        "ultros_site.tasks.builds",
-        "ultros_site.tasks.common",
-        "ultros_site.tasks.discord",
-        "ultros_site.tasks.email",
-        "ultros_site.tasks.nodebb",
-        "ultros_site.tasks.notify",
-        "ultros_site.tasks.scheduled",
-        "ultros_site.tasks.twitter"
-    ]
+    broker=config.broker,
+    backend=config.backend,
+    include=config.include
 )
 
-app.conf.beat_schedule = {
-    "clean_sessions": {
-        "task": "scheduled_clean_sessions",
-        "schedule": crontab(hour=0, minute=0),
-        "args": ()
-    },
-
-    "clean_users": {
-        "task": "scheduled_clean_users",
-        "schedule": crontab(hour=1, minute=0),
-        "args": ()
-    },
-
-    "clean_tasks": {
-        "task": "scheduled_clean_tasks",
-        "schedule": crontab(hour=2, minute=0),
-        "args": ()
-    }
-}
+app.conf.beat_schedule = config.beat_schedule
 
 
 if __name__ == "__main__":
